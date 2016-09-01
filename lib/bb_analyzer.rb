0d3ecf05723e48ce21dbaebe5493902e1d506567
Kris Dekeyser <kris.dekeyser@libis.be>
2016-09-01 20:14:21 +0200

    Update with latest analysis algorithm

--- lib/bb_analyzer.rb
+++ lib/bb_analyzer.rb
@@ -10,27 +10,31 @@ class BbAnalyzer
     @collections = {collections: {}, ies: []}
     @ie_list = Array.new
     @file_list = Set.new
-    @html_not_found = Set.new
-    @html_duplicate = Set.new
+    @html_not_found = Array.new
+    @html_duplicate = Array.new
+    @bad_filenames = Array.new
     get_files Pathname.new('.')
     @file_list_dup = @file_list.dup
     process_csv(csv)
-    # puts 'Files found:'
-    # @file_list.sort.each { |f| puts " - #{f}" }
-#    puts 'IEs:'
-#    print_collection @collections
-#    dis = discrepancies
-#    puts 'Discrepanties:'
-#    if dis[:not_found].size > 0
-#      puts ' + Verwijzigen in HTML, maar niet gevonden:'
-#      dis[:not_found].each { |f| puts "   - #{f}"}
-#    end
-#    if dis[:unreferenced].size > 0
-#      puts ' + Bestanden gevonden, zonder verwijzing in een HTML bestand (en dus niet opgenomen):'
-#      dis[:unreferenced].each { |f| puts "   - #{f}"}
-#    end
-    # puts ' + ignored:'
-    # dis[:ignored].each { |f| puts "   - #{f}"}
+    puts 'IEs:'
+    print_collection @collections
+    dis = discrepancies
+    puts 'Discrepanties:'
+    if dis[:not_found].size > 0
+      puts ' + Verwijzigen in HTML, maar niet gevonden:'
+      dis[:not_found].each { |f| puts "   - #{f[:dir].to_s} / #{f[:html].to_s} : #{f[:link]}" }
+    end
+    if dis[:unreferenced].size > 0
+      puts ' + Bestanden gevonden, zonder verwijzing in een HTML bestand (en dus niet opgenomen):'
+      dis[:unreferenced].each { |f| puts "   - #{f}" }
+    end
+    if dis[:double_referenced].size > 0
+      puts ' + Bestanden met referenties uit meer dan 1 HTML bestand (en dus meerdere keren opgeladen):'
+      dis[:double_referenced].each do |k, v|
+        puts "   - #{k.to_s}"
+        v.each { |f| puts "     - #{f[:dir].to_s}/#{f[:html].to_s}" }
+      end
+    end
     ie_to_csv
     discr_to_csv
   end
@@ -38,12 +42,12 @@ class BbAnalyzer
   def ie_to_csv
     CSV.open('ie_list.csv', 'wt') do |csv|
       csv << ['Collectie', 'IE naam (dc:title)', 'Bestand', 'Opmerking']
-      @ie_list.each do |ie|
+      @ie_list.sort_by {|f| (f[:path] + f[:filename]).to_s}.each do |ie|
         csv << [ie[:path].to_s, ie[:title], ie[:filename].to_s]
-        ie[:links].each do |link|
+        ie[:links].sort.each do |link|
           csv << ['', '', link, (@root_dir + ie[:path] + link).exist? ? '' : '** BESTAND ONTBREEKT !! **']
         end
-        ie[:images].each do |link|
+        ie[:images].sort.each do |link|
           csv << ['', '', link, (@root_dir + ie[:path] + link).exist? ? '' : '** BESTAND ONTBREEKT !! **']
         end
       end
@@ -54,15 +58,17 @@ class BbAnalyzer
     disc = discrepancies
     CSV.open('notfound.csv', 'wt') do |csv|
       csv << %w(Folder Html Link)
-      disc[:not_found].each { |f| csv << [f[:dir].to_s, f[:html].to_s , f[:link].to_s] }
-    end
-    CSV.open('ignored.csv', 'wt') do |csv|
-      csv << %w(Folder Html Link)
-      disc[:ignored].each { |f| csv << [f[:dir].to_s, f[:html].to_s , f[:link].to_s] }
+      disc[:not_found].each { |f| csv << [f[:dir].to_s, f[:html].to_s, f[:link].to_s] }
     end
     CSV.open('notreferenced.csv', 'wt') do |csv|
       csv << %w(Folder Bestandsnaam)
       disc[:unreferenced].each { |f| csv << [f.dirname.to_s, f.basename.to_s] }
+    end
+    CSV.open('doublereferenced.csv', 'wt') do |csv|
+      csv << %w(Folder Bestandsnaam HTML_bestand)
+      disc[:double_referenced].each do |_, v|
+        v.each { |f| csv << [f[:dir].to_s, f[:link].to_s, f[:html].to_s] }
+      end
     end
     CSV.open('html_notfound.csv', 'wt') do |csv|
       csv << %w(Folder Html)
@@ -76,6 +82,12 @@ class BbAnalyzer
         csv << [f.dirname.to_s, f.basename]
       end
     end
+    CSV.open('bad_files.csv', 'wt') do |csv|
+      csv << %w(BadName RenameTo)
+      @bad_filenames.each do |f|
+        csv << ["#{File.join(f[:path], f[:parts].join('?'))}", f[:new_name] || '???']
+      end
+    end
   end
 
   protected
@@ -83,30 +95,38 @@ class BbAnalyzer
   def get_files(rel_path)
     abs_path = @root_dir + rel_path
     abs_path.entries.each do |name|
-      rel_name = rel_path + name
-      abs_name = abs_path + name
-      case
-        when abs_name.directory?
-          next if rel_name == rel_path
-          next if rel_name == rel_path.parent
-          get_files rel_name
-        when abs_name.file?
-          process_file rel_name
-        else
-          #ignore
+      begin
+        rel_name = rel_path + name
+        abs_name = abs_path + name
+        case
+          when abs_name.directory?
+            next if rel_name == rel_path
+            next if rel_name == rel_path.parent
+            get_files rel_name
+          when abs_name.file?
+            process_file rel_name
+          else
+            #ignore
+        end
+      rescue
+        nameparts = name.to_s.encode(replace: '|', undef: :replace, invalid: :replace).split('|')
+        @bad_filenames << {path: rel_path.to_s, bad_name: name.to_s, parts: nameparts}
       end
     end
   end
 
   def process_csv(path = nil)
-    if path 
-      CSV.open(path, headers: true) do |csv|
+    if path
+      CSV.open(path, 'rb:windows-1252:UTF-8', headers: true) do |csv|
+        # CSV.open(path, 'rb', headers: true) do |csv|
         csv.each do |row|
           fname = Pathname.new(row[5].gsub(/^c:\\export\\/, '').gsub(/\\/, '/'))
           if @file_list.delete?(fname)
             process_ie fname
           elsif @file_list_dup.include?(fname)
             @html_duplicate << fname
+            # elsif find_bad(fname.parent.to_s, fname.basename.to_s)
+            #   process_ie fname
           else
             @html_not_found << fname
           end
@@ -120,7 +140,7 @@ class BbAnalyzer
           to_delete << fpath
         end
       end
-      to_delete.each {|fpath| @file_list.delete fpath}
+      to_delete.each { |fpath| @file_list.delete fpath }
     end
   end
 
@@ -148,10 +168,18 @@ class BbAnalyzer
     end
   end
 
+  def ignore_file(rel_name)
+    rel_name.to_s =~ /icons\/.*\.gif/ || rel_name.to_s =~ /\/(TempBody.*|graycol)\.(gif|jpg)$/
+  end
+
+  def ignore_link(link)
+    return true if link =~ /^(mailto:|http:)/
+    ignore_file(link)
+  end
+
   # @param [Pathname] rel_name
   def process_file(rel_name)
-    return if rel_name.to_s == 'icons/graycol.gif'
-    #return if rel_name.to_s =~ /\/(TempBody.*|graycol)\.gif$/
+    return if ignore_file(rel_name)
     @file_list << rel_name.cleanpath
   end
 
@@ -163,16 +191,8 @@ class BbAnalyzer
     html = Nokogiri::HTML(f) { |config| config.strict.nonet.noblanks }
     f.close
     # Attachments
-    links = html.xpath('//a/@href').map(&:value).map {|link| link2path(link)}
-    links_ignored = links.select { |link| link =~ /^(mailto:|http:)/ }
-    links_ok = links - links_ignored
-    #links_ok = links_ok.map { |link| cleanup(link, rel_path) }
-    images = html.xpath('//img/@src').map(&:value).map {|link| link2path(link)}
-    images_ok = images.reject {|i| i.to_s == 'icons/graycol.gif'}
-    #images_ok = images.reject {|i| i =~ /\/(TempBody.*|graycol)\.gif$/}
-    images_ignored = images - images_ok
-    #images_ok = images_ok.map { |link| cleanup(link, rel_path) }
-    #images_ignored = images_ignored.map { |link| cleanup(link, rel_path) }
+    links = html.xpath('//a/@href').map(&:value).map { |link| link2path(link) }.reject { |link| ignore_link(link) }
+    images = html.xpath('//img/@src').map(&:value).map { |link| link2path(link) }.reject { |i| ignore_file(i) }
     # Title
     titles = html.css('div table tr td div span strong').map(&:content)
     # Store result
@@ -180,10 +200,8 @@ class BbAnalyzer
         path: rel_path,
         filename: rel_name.basename,
         title: titles.first.gsub(/[\r\n]/, ''),
-        links: links_ok,
-        ignored_links: links_ignored,
-        images: images_ok,
-        ignored_images: images_ignored
+        links: links,
+        images: images,
     }
     @ie_list << ie
     bucket = rel_path.each_filename.to_a.inject(@collections) do |bucket, dir|
@@ -194,21 +212,24 @@ class BbAnalyzer
 
   def discrepancies
     files_unreferenced = @file_list.dup
+    files_referenced = Hash.new
     files_notfound = Set.new
-    files_ignored = Set.new
     @ie_list.each do |ie|
-      ie[:links].each do |file|
-        files_notfound << { link: file, dir: ie[:path], html: ie[:filename] } unless files_unreferenced.delete?(ie[:path] + file)
-      end
-      ie[:images].each do |file|
-        files_notfound << { link: file, dir: ie[:path], html: ie[:filename] } unless files_unreferenced.delete?(ie[:path] + file)
-      end
-      ie[:ignored_links].each do |file|
-        files_unreferenced.delete(file)
-        files_ignored << { dir: ie[:path], html: ie[:filename], link: file }
+      (ie[:links] + ie[:images]).each do |file|
+        path = ie[:path] + file
+        files_notfound << {link: file, dir: ie[:path], html: ie[:filename]} unless @file_list.include?(path)
+        files_unreferenced.delete(path)
+        files_referenced[path.to_s] ||= []
+        unless files_referenced[path.to_s].any? { |x| x[:html] == ie[:filename] }
+          files_referenced[path.to_s] << {link: file, dir: ie[:path], html: ie[:filename]}
+        end
       end
     end
-    {not_found: files_notfound, unreferenced: files_unreferenced, ignored: files_ignored}
+    {
+        not_found: files_notfound.sort_by {|f| (f[:dir] + f[:link]).to_s},
+        unreferenced: files_unreferenced.sort_by {|f| f.to_s },
+        double_referenced: files_referenced.select { |_, v| v.size > 1 }.sort,
+    }
   end
 
   private
@@ -223,23 +244,45 @@ class BbAnalyzer
   end
 
   def print_collection(collection, indent = 0)
-    collection[:collections].each do |name, content|
+    collection[:collections].sort.each do |name, content|
       puts "#{' ' * indent} + #{name}/"
       print_collection(content, indent + 2)
     end
-    collection[:ies].each do |ie|
+    collection[:ies].sort_by {|f| (f[:path] + f[:filename]).to_s}.each do |ie|
       print_ie ie, indent
     end
   end
 
   def print_ie(ie, indent = 0)
-    puts "#{' ' * indent} - #{ie[:title]}"
+    puts "#{' ' * indent} - #{ie[:title]} [#{ie[:filename].to_s}]"
     ie[:links].each do |link|
       puts "#{' ' * indent}   . #{ '** BESTAND ONTBREEKT !! ** ' unless (@root_dir + ie[:path] + link).exist?}#{link}"
     end
   end
 
-
+  def find_bad(path, file)
+    path = File.join(path, File.dirname(file)) unless File.dirname(file) == '.'
+    file = File.basename(file)
+    found = nil
+    @bad_filenames.each do |entry|
+      next unless path == entry[:path]
+      regex = /^#{entry[:parts].map { |e| Regexp.quote(e) }.join('.')}$/
+      regex = /^#{Regexp.quote(entry[:new_name])}$/ if entry[:new_name]
+      if file =~ regex
+        if found
+          puts "ERROR: found multiple bad_name matches for #{File.join(path, file)}:"
+          puts " - #{found}"
+          puts " - #{entry}"
+          return nil
+        end
+        puts "rename: #{File.join(path, entry[:bad_name])} to #{File.join(path, file)}" unless entry[:new_name]
+        # File.rename(File.join(@root_dir, path, entry[:bad_name]), File.join(@root_dir, path, file))
+        entry[:new_name] = file
+        found = entry
+      end
+    end
+    found
+  end
 
 end
 
